fcd41f1c527e1264dc08c6bd48b351658e1bddbd
rampellipradeep <rampelli.pradeep@gmail.com>
2017-02-05 10:40:59 +0530

    changed referal url , changed host

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -7,11 +7,38 @@ chrome.tabs.onUpdated.addListener(tabId => {
  // console.log('previousVersion', tabId);
   //chrome.pageAction.show(tabId);
 });
+
+chrome.cookies.get({url: 'https://www.flipkart.com', name:'AFFID'}, function(res){
+  if(res && res.value == 'rampellip'){
+    chrome.browserAction.setBadgeText({text: "Y"});
+  }else{
+    chrome.browserAction.setBadgeText({text: ""});
+  }
+})
+
+ function getURLParmsObj(query){
+  query = query.replace('?','');
+  var params = query.split('&');
+  var paramObj = params.map(function(item){
+    var values = item.split('=');
+    var obj = {};
+    obj[values[0]] = values[1];
+   return obj;
+  });
+  return paramObj;
+ }
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {
     if(info.type === 'main_frame' && info.url.indexOf('rampellip') <= -1){
       var url = info.url;
-      var redirect = appendReferalURL(url);
+      var parser = document.createElement('a');
+          parser.href = url;
+          if(parser.search != ''){
+            var queryParam = parser.search;
+            var queryObj = getURLParmsObj(queryParam);
+          }
+      var modefiedUrl = url.replace('www.flipkart.com','dl.flipkart.com/dl');
+      var redirect = appendReferalURL(modefiedUrl);
       return {redirectUrl: redirect};
     }
   },
